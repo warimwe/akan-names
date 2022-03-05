9638ccd376f7c959f915293ffe4e13a85e58135c
Annette Warimwe <annettewarimwe@gmail.com>
2022-03-05 11:30:51 +0300

    Get the date and gender values from html

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-function checkAkanName() {}
+function checkAkanName() {
+  var dayOfBirth = document.date_form.dayOfBirth.value;
+  var monthOfBirth = document.date_form.monthOfBirth.value;
+  var yearOfBirth = document.date_form.yearOfBirth.value;
+  var gender = document.date_form.gender.value;
+  alert(dayOfBirth + monthOfBirth + yearOfBirth + gender);
+}
