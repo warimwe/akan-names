1a4b127c982532496663c58a8d0546d458cd0422
Annette Warimwe <annettewarimwe@gmail.com>
2022-03-05 11:33:01 +0300

    Create arrays for male and female Akan names and the respective days of the week

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,33 @@ function checkAkanName() {
   var monthOfBirth = document.date_form.monthOfBirth.value;
   var yearOfBirth = document.date_form.yearOfBirth.value;
   var gender = document.date_form.gender.value;
-  alert(dayOfBirth + monthOfBirth + yearOfBirth + gender);
+  // alert(dayOfBirth + monthOfBirth + yearOfBirth + gender);
+
+  var weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var maleAkanNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ];
+  var femaleAkanNames = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+  ];
 }
